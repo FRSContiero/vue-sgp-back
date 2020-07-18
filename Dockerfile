e2fdd7f301686c15cdb21cd3afc186755a1861e8
Fábio Contiero <frscontiero@gmail.com>
2020-07-17 21:56:21 -0300

    Equivalendo ao final do projeto.

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,8 @@ FROM node:10.15.3-alpine
 
 WORKDIR /usr/app
 
-COPY  package*.json ./
+COPY package*.json ./
 
 RUN npm install
 
 COPY . .
-
-EXPOSE 4000
-
-CMD ["npm" , "run", "dev"]
--- src/index.js
+++ src/index.js
@@ -2,22 +2,26 @@ const { GraphQLServer } = require('graphql-yoga')
 const Binding = require('prisma-binding')
 const { prisma } = require('./generated/prisma-client')
 
-const binding = new Binding.Prisma({
-  typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
-  endpoint: process.env.PRISMA_ENDPOINT
-})
-
-const resolvers = {
-  Query: {
-    user(parent, args, context, info) {
-      return prisma.user({ id: args.id })
-    }
-  }
-}
+const { endpoint, origin, playground, secret } = require('./config')
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
-  resolvers 
+  resolvers,
+  context: request => ({
+    ...request,
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint,
+      secret
+    }),
+    prisma
+  })
 })
 
-server.start().then(() => console.log('Server running on http://localhost:4000'))
+server.start({
+  playground,
+  cors: {
+    origin
+  }
+}).then(() => console.log('Server running on http://localhost:4000...'))
